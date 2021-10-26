67778dbb560e7c9714a98b3b8f4bcc17c0ab5305
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-10-26 18:52:04 +0200

    Add link to view maps at ray1map, move urls to constant properties

---
+++ src/KlonoaHeroesPatcher/App/URLs.cs
@@ -0,0 +1,11 @@
+﻿namespace KlonoaHeroesPatcher
+{
+    public static class URLs
+    {
+        public const string Ray1Map_KlonoaHeroes = "https://raym.app/maps_r1/?mode=KlonoaHeroesGBAJP&folder=klonoa_heroes/heroes_jp";
+
+        public const string GitHub_BinarySerializer_Klonoa = "https://github.com/RayCarrot/BinarySerializer.Klonoa";
+
+        public const string GitHub_KlonoaHeroesPatcher = "https://github.com/RayCarrot/KlonoaHeroesPatcher";
+    }
+}
